1f24b899e1e8f4e52b39275475636294ed41e840
urboapp <55747649+urboapp@users.noreply.github.com>
2022-09-02 08:42:02 -0300

    update 1

--- 01-Fundamentos-Reatcjs/src/App.jsx
+++ 01-Fundamentos-Reatcjs/src/App.jsx
@@ -1,5 +1,15 @@
-function App() {
-  return <h1>Hello word</h1>;
-}
+import { Post } from "./Post";
 
-export default App;
+export function App() {
+  return (
+    <div>
+      <Post
+        author="John"
+        content="orem ipsum dolor sit amet consectetur adipisicing elit. 
+                     Et temporibus incidunt voluptates quae eum modi non beatae! Qui ullam voluptate iusto,
+                    ducimus ut architecto ex hic eligendi perferendis molestiae consequatur!"
+      />
+      <Post author="André" content="Construindo componentes reutilizaveis" />
+    </div>
+  );
+}
